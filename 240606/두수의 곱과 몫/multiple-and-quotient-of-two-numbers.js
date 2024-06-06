b885d89e03631b402ea7e5481c65826c34d3effd
tacowasabii <godseoril812@gmail.com>
2024-06-06 18:21:36 +0900

    Title: 두수의 곱과 몫, Time: 192ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240606/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+a = Number(input[0])
+b = Number(input[1])
+
+console.log(`${a} * ${b} = ${a * b}`)
+console.log(`${a} / ${b} = ${a / b}`)
