8e1b7aca04640ffe59830836b31c490217519008
tacowasabii <godseoril812@gmail.com>
2024-06-07 22:47:24 +0900

    Title: 비교에 따른 연산, Time: 243ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240607/비교에 따른 연산/operation-based-on-comparison.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+let arr = fs.readFileSync(0).toString().trim().split(" ");
+a = Number(arr[0])
+b = Number(arr[1])
+
+if (a > b)
+    console.log(a * b)
+else
+    console.log(parseInt(b / a))
