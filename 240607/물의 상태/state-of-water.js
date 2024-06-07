5c76a7cfa00450e099715eccd3f66686f1a7f7a7
tacowasabii <godseoril812@gmail.com>
2024-06-07 22:56:01 +0900

    Title: 물의 상태, Time: 219ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240607/물의 상태/state-of-water.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let a = Number(fs.readFileSync(0).toString().trim());
+
+if (a < 0) {
+    console.log("ice");
+} 
+else if (a >= 100) {
+    console.log("vapor");
+} 
+else {
+    console.log("water");
+}
