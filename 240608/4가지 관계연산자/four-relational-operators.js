9686e1b7781ba7b81572755f6705c37369117507
tacowasabii <godseoril812@gmail.com>
2024-06-08 16:25:19 +0900

    Title: 4가지 관계연산자, Time: 185ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240608/4가지 관계연산자/four-relational-operators.js
@@ -0,0 +1,22 @@
+const fs = require("fs")
+let arr = fs.readFileSync(0).toString().trim().split(" ")
+
+a = Number(arr[0])
+b = Number(arr[1])
+
+if (a >= b)
+    console.log(1)
+else
+    console.log(0)
+if (a > b)
+    console.log(1)
+else
+    console.log(0)
+if (b >= a)
+    console.log(1)
+else
+    console.log(0)
+if (b > a)
+    console.log(1)
+else
+    console.log(0)
