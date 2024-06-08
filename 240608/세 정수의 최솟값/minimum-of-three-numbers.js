7e76610b77ed21223c30905c7bf155b3cb80cd8d
tacowasabii <godseoril812@gmail.com>
2024-06-08 16:47:24 +0900

    Title: 세 정수의 최솟값, Time: 187ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240608/세 정수의 최솟값/minimum-of-three-numbers.js
@@ -0,0 +1,16 @@
+const fs = require("fs")
+let arr = fs.readFileSync(0).toString().trim().split(" ")
+
+let a = Number(arr[0])
+let b = Number(arr[1])
+let c = Number(arr[2])
+
+let min;
+
+if (a < b && a < c)
+    min = a
+else if (a < b && c < a)
+    min = c
+else 
+    min = b
+console.log(min)
