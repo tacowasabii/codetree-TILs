c93512fb0e29b2c04b1cb2351f207a31d9b892ad
tacowasabii <godseoril812@gmail.com>
2024-06-09 17:44:27 +0900

    Title: 일 수 구하기, Time: 238ms, Memory: 14MB, Status: Passed - CodeTree

--- 240609/일 수 구하기/number-of-days-in-month.js
+++ 240609/일 수 구하기/number-of-days-in-month.js
@@ -1,9 +1,22 @@
 const fs = require("fs");
 let n = Number(fs.readFileSync(0).toString().trim());
 
-if (n === 2)
-    console.log(28)
-else if (n === 1 || n === 3 || n === 5 || n === 7 || n === 8 || n === 10 || n === 12)
-    console.log(31)
-else
-    console.log(30)
+if (n === 2) {
+    console.log(28);
+}
+else if (n <= 7) {
+    if (n % 2 === 1) {
+        console.log(31);
+    }
+    else {
+        console.log(30);
+    }
+}
+else {
+    if (n % 2 === 0) {
+        console.log(31);
+    }
+    else {
+        console.log(30);
+    }
+}
