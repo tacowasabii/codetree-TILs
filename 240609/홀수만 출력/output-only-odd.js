325e64bb72450a97ae7c23be256e6965340a2ce5
tacowasabii <godseoril812@gmail.com>
2024-06-09 18:16:51 +0900

    Title: 홀수만 출력, Time: 233ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240609/홀수만 출력/output-only-odd.js
@@ -0,0 +1,14 @@
+const fs = require("fs")
+let arr = fs.readFileSync(0).toString().trim().split(" ")
+
+let a = Number(arr[0])
+let b = Number(arr[1])
+
+let result = ""
+
+for (let i = a; i <= b; i++){
+    if (i % 2 !== 0)
+        result += (i + " ")
+}
+
+console.log(result)
