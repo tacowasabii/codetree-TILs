3144fafe83942f3dd7b01f35964d82ab8f96df6b
tacowasabii <godseoril812@gmail.com>
2024-06-10 23:42:06 +0900

    Title: 100점 도달하기, Time: 194ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240610/100점 도달하기/reach-100-point.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+
+let result = ""
+
+for (i = n; i <= 100; i++){
+    if (i >= 90)
+        result += "A "
+    else if (i >= 80)
+        result += "B "
+    else if (i >= 70)
+        result += "C "
+    else if (i >= 60)
+        result += "D "
+    else
+        result += "F "
+}
+
+console.log(result);
