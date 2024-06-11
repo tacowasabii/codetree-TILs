68a087c9cf99c1944fe0bbe0d9ab37ce4cbed663
tacowasabii <godseoril812@gmail.com>
2024-06-11 17:33:58 +0900

    Title: 총합 구하기, Time: 325ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240611/총합 구하기/find-the-total.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+let arr = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(arr[0]), b = Number(arr[1])
+let sum = 0
+
+for (i = a; i <= b; i++){
+    if (i % 6 === 0 && i % 8 !== 0)
+        sum += i
+}
+
+console.log(sum)
