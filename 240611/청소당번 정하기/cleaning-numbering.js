a1e4b5666b8d3fea433bccfad8cbe2bb12ffc9e2
tacowasabii <godseoril812@gmail.com>
2024-06-11 16:57:54 +0900

    Title: 청소당번 정하기, Time: 202ms, Memory: 15MB, Status: Passed - CodeTree

--- 240611/청소당번 정하기/cleaning-numbering.js
+++ 240611/청소당번 정하기/cleaning-numbering.js
@@ -5,11 +5,11 @@ let cnt3 = 0
 let cnt12 = 0
 
 for (let i = 1; i <= n; i++) {
-        if (i >= 12 && i % 12 == 0)
+        if (i % 12 === 0)
             cnt12 ++;
-        else if (i >= 3 && i % 3 == 0)
+        else if (i % 3 === 0)
             cnt3 ++;
-        else if (i >= 2 && i % 2 == 0)
+        else if (i % 2 === 0)
             cnt2 ++;
     }
 
