9edfefe03fdf42c028300019c53f555c502d5223
tacowasabii <godseoril812@gmail.com>
2024-06-12 18:28:30 +0900

    Title: 1부터 곱셈하기, Time: 236ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240612/1부터 곱셈하기/multiple-from-one.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+
+let prod = 1
+
+for (i = 1; i <= 10; i++){
+    prod *= i
+    if (prod >= n){
+        console.log(i)
+        break
+    }
+}
