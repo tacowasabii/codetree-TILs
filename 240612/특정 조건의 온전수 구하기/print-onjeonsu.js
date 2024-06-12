3165eff7c45307649e038894c747792130ba3028
tacowasabii <godseoril812@gmail.com>
2024-06-12 18:22:53 +0900

    Title: 특정 조건의 온전수 구하기, Time: 193ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240612/특정 조건의 온전수 구하기/print-onjeonsu.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+
+let result = ""
+
+for (i = 1; i <= n; i++){
+    if (i % 2 === 0 || i % 10 === 5 || i % 3 === 0 && i % 9 !== 0)
+        continue
+    result += i + " "
+}
+
+console.log(result)
