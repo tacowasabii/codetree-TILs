b32ad6392d998fde47dc6f108396e27530734f1c
tacowasabii <godseoril812@gmail.com>
2024-06-12 22:09:06 +0900

    Title: 3n + 1 수열, Time: 189ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240612/3n + 1 수열/3n-plus-1-sequence.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+
+let cnt = 0
+
+while (n !== 1) {
+    if (n % 2 === 0 )
+        n /= 2
+    else
+        n = n * 3 + 1
+    cnt ++
+}
+console.log(cnt);
