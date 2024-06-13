8069391ebaa8097b13184ca62df563c18a5773de
tacowasabii <godseoril812@gmail.com>
2024-06-13 19:44:46 +0900

    Title: 소수 판별기, Time: 192ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240613/소수 판별기/prime-number-discriminator.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+
+let satisfied = false;
+
+for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+        satisfied = true;
+    }
+}
+
+if (satisfied === true) {
+    console.log("C");
+}
+else {
+    console.log("P");
+}
