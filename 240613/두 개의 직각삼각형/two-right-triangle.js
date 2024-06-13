6fb0f70417866a5dc40e37113fb4bf10a76c5ebb
tacowasabii <godseoril812@gmail.com>
2024-06-13 20:16:40 +0900

    Title: 두 개의 직각삼각형, Time: 177ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240613/두 개의 직각삼각형/two-right-triangle.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+
+let str = "";
+
+for (let i = n; i > 0; i--) {
+    str = "";
+    for (let j = 0; j < i; j++) {
+        str += "*";
+    }
+    for (let j = 0; j < n - i; j++) {
+        str += "  ";
+    }
+    for (let j = 0; j < i; j++) {
+        str += "*";
+    }
+    console.log(str);
+}
