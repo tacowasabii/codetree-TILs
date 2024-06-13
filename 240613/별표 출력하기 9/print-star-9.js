b7f537dda99c567b86e5ee329464cbba56558c1a
tacowasabii <godseoril812@gmail.com>
2024-06-13 20:12:28 +0900

    Title: 별표 출력하기 9, Time: 173ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240613/별표 출력하기 9/print-star-9.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+
+let str = "";
+
+for (let i = 0; i < n; i++) {
+    str = "";
+    for (let j = 1; j < n - i; j++) {
+        str += "  ";
+    }
+    for (let j = 0; j < 2 * i + 1; j++) {
+        str += "* ";
+    }
+    console.log(str);
+}
