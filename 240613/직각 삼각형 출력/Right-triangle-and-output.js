8983634b8a2c32412aca737ccf26c5784ee2b738
tacowasabii <godseoril812@gmail.com>
2024-06-13 20:00:11 +0900

    Title: 직각 삼각형 출력, Time: 227ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240613/직각 삼각형 출력/Right-triangle-and-output.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+
+let str = "";
+
+for (let i = 0; i < n; i++) {       
+    str = "";
+    for (let j = 0; j <= i * 2; j++) {  
+        str += "*";
+    }
+    console.log(str);
+}
