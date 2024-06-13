b9182c6327674b434cf9342f6ccf81cecc021526
tacowasabii <godseoril812@gmail.com>
2024-06-13 20:07:21 +0900

    Title: 건너뛰고 별표 출력하기, Time: 226ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240613/건너뛰고 별표 출력하기/skip-and-print-a-star.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+
+let str = "";
+
+
+for (let i = 0; i < n; i++) {
+    str = "";
+    for (let j = 0; j <= i; j++) {
+        str += "*";
+    }
+    console.log(str);
+}
+
+for (let i = n; i > 0; i--) {
+    str = "";
+    for (let j = 0; j < i - 1; j++) {
+        str += "*";
+    }
+    console.log(str);
+    console.log()
+}
