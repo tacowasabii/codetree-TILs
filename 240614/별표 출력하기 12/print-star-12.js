160e07568b7bee77d926d8f86254a399ccc2a481
tacowasabii <godseoril812@gmail.com>
2024-06-14 22:14:24 +0900

    Title: 별표 출력하기 12, Time: 192ms, Memory: 14MB, Status: Passed - CodeTree

--- 240614/별표 출력하기 12/print-star-12.js
+++ 240614/별표 출력하기 12/print-star-12.js
@@ -1,14 +1,25 @@
+// 변수 선언 및 입력
 const fs = require("fs");
 let n = Number(fs.readFileSync(0).toString().trim());
 
+let str = "";
+
+// 모양에 맞게 별을 출력합니다.
 for (let i = 0; i < n; i++) {
-    let str = "";
+    str = "";
     for (let j = 0; j < n; j++) {
-        if (i === j || i % 2 === 0 && j % 2 === 0) {
-            str += "* ";
-        } else {
-            str += "  ";
+        if (j % 2 === 0) {
+            if (i === 0)
+                str += "* ";
+            else
+                str += "  ";
+        }
+        else {
+            if (i <= j)
+                str += "* ";
+            else
+                str += "  ";
         }
     }
-    console.log(str.trim());
+    console.log(str);
 }
