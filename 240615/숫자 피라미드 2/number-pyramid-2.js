cfaa589d53c93e18c571eb8419bb004cb17ae571
tacowasabii <godseoril812@gmail.com>
2024-06-15 18:26:21 +0900

    Title: 숫자 피라미드 2, Time: 170ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240615/숫자 피라미드 2/number-pyramid-2.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+
+let str = "";
+let cnt = 1
+
+for (let i = 0; i < n; i++) {
+    str = "";
+    for (let j = 0; j < i + 1; j++) {
+        str += cnt + " ";
+        cnt++
+    }
+    console.log(str);
+}
