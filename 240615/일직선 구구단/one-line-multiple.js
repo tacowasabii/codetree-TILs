83e75ab512e134ce594b4ca5f8485d00ee30dc81
tacowasabii <godseoril812@gmail.com>
2024-06-15 18:40:11 +0900

    Title: 일직선 구구단, Time: 199ms, Memory: 14MB, Status: Passed - CodeTree

--- 240615/일직선 구구단/one-line-multiple.js
+++ 240615/일직선 구구단/one-line-multiple.js
@@ -1,8 +1,6 @@
 const fs = require("fs");
 let n = Number(fs.readFileSync(0).toString().trim());
 
-let str = "";
-
 for (let i = 1; i <= n; i++) {
     for (let j = 1; j <= n; j++) {
         console.log(`${i} * ${j} = ${i * j}`);
