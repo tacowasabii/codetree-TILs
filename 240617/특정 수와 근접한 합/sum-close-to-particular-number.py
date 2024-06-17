b7c316fe838a58fa865766bb94545b6a3e7d2a08
tacowasabii <godseoril812@gmail.com>
2024-06-17 23:49:12 +0900

    Title: 특정 수와 근접한 합, Time: 140ms, Memory: 28MB, Status: Passed - CodeTree

--- 240617/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 240617/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -1,17 +1,12 @@
 from itertools import combinations
 
-N, S = map(int, input().strip().split())
-arr_input = input().strip()
+N, S = map(int, input().split())
+arr = list(map(int, input().split()))
 
-if arr_input:
-    arr = list(map(int, arr_input.split()))
-else:
-    arr = []
-
-combs = list(combinations(arr, N - 2))
+combs = list(combinations(arr, N-2))
 diffs = []
 
-for comb in combs:
-    diffs.append(abs(S - sum(comb)))
+for i in combs:
+    diffs.append(abs(S - sum(i)))
 
 print(min(diffs))
