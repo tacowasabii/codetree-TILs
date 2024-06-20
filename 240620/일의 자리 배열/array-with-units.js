5af72131254e81650cb04ac28bdb5fa6448c52e6
tacowasabii <godseoril812@gmail.com>
2024-06-20 23:41:47 +0900

    Title: 일의 자리 배열, Time: 235ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240620/일의 자리 배열/array-with-units.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+let arr = [input[0], input[1]]
+
+
+for (let i = 2; i < 10; i++){
+    arr.push((arr[i - 2] + arr[i - 1]) % 10)
+}
+
+console.log(arr.join(" "))
