bd492459fb0b57e22437058456c5a4062194fe5a
tacowasabii <godseoril812@gmail.com>
2024-06-22 12:00:24 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 183ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240622/중복되지 않는 숫자 중 최대/max-of-unique-number.js
+++ 240622/중복되지 않는 숫자 중 최대/max-of-unique-number.js
@@ -1,13 +1,21 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split("\n");
 let arr = input[1].split(" ").map(Number)
-let max = -1
-let nums = []
+let frequency = {};
 
-for (let elem of arr){
-    if (!nums.includes(elem))
-        max = Math.max(max, elem)
-        nums.push(elem)
+for (let elem of arr) {
+    if (frequency[elem]) {
+        frequency[elem]++;
+    } else {
+        frequency[elem] = 1;
+    }
 }
 
-console.log(max)
+let maxUnique = -1;
+for (let num in frequency) {
+    if (frequency[num] === 1 && num > maxUnique) {
+        maxUnique = num;
+    }
+}
+
+console.log(maxUnique)
