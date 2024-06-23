4378465fa2900ef8e0a828eedbc56eabc77685be
tacowasabii <godseoril812@gmail.com>
2024-06-23 22:03:51 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 219ms, Memory: 15MB, Status: Passed - CodeTree

--- 240623/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.js
+++ 240623/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.js
@@ -2,18 +2,14 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split("\n");
 let n = Number(input[0])
 let arr = input[1].split(" ").map(Number);
-let profit = 0
-let price = arr[0]
+let min = arr[0]
 let profits = [0]
 
 for (let i = 1; i < n; i++){
-    if (arr[i] > price){
-        profit += arr[i] - price
-        profits.push(profit)
-    } else if (arr[i] < price){
-        profit = 0
-    }
-    price = arr[i]
+    if (arr[i] > min)
+        profits.push(arr[i] - min)
+    else if (arr[i] < min)
+        min = arr[i]
 }
 
 console.log(Math.max(...profits))
