c2a9c4f4fd52f84cafc2f25c7817a8109011c34e
tacowasabii <godseoril812@gmail.com>
2024-06-24 21:34:59 +0900

    Title: 격자로 사각형 만들기, Time: 174ms, Memory: 15MB, Status: Passed - CodeTree

--- 240624/격자로 사각형 만들기/print-grid-in-rectangle.js
+++ 240624/격자로 사각형 만들기/print-grid-in-rectangle.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 let n = Number(fs.readFileSync(0).toString().trim());
 let arr2d = Array(n).fill(0).map(() => Array(n).fill(0));
 
-for (let i = 0; i < 5; i++){
-    for (let j = 0; j < 5; j++){
+for (let i = 0; i < n; i++){
+    for (let j = 0; j < n; j++){
         if (i === 0 || j === 0)
             arr2d[i][j] = 1
         else
