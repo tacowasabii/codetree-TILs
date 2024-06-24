3f0776a2b46d7365d7d79ea939a72a28c0e21a4f
tacowasabii <godseoril812@gmail.com>
2024-06-24 21:34:11 +0900

    Title: 격자로 사각형 만들기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240624/격자로 사각형 만들기/print-grid-in-rectangle.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+let cnt = 1
+let arr2d = Array(n).fill(0).map(() => Array(cnt++).fill(0));
+
+for (let i = 0; i < 5; i++){
+    for (let j = 0; j < 5; j++){
+        if (i === 0 || j === 0)
+            arr2d[i][j] = 1
+        else
+            arr2d[i][j] = arr2d[i - 1][j] + arr2d[i][j - 1] + arr2d[i - 1][j - 1]
+    }
+}
+
+for (let elem of arr2d){
+    console.log(elem.join(" "))
+}
