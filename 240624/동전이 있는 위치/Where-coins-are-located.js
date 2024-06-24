7a86f50b5236e670dd0faca506ac8f44300fcaaf
tacowasabii <godseoril812@gmail.com>
2024-06-24 21:44:05 +0900

    Title: 동전이 있는 위치, Time: 240ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240624/동전이 있는 위치/Where-coins-are-located.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+let [n, m] = input[0].split(" ").map(Number)
+let arr2d = Array(n).fill(0).map(() => Array(n).fill(0));
+
+for (let i = 1; i <= m; i ++){
+    let [r, c] = input[i].split(" ").map(Number)
+    arr2d[r - 1][c - 1] = 1
+}
+
+for (let elem of arr2d){
+    console.log(elem.join(" "))
+}
