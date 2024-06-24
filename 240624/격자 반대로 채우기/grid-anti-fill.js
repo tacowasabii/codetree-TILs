7f93b1251bac094356ca76fc5e9b1782cd35ddfb
tacowasabii <godseoril812@gmail.com>
2024-06-24 21:13:31 +0900

    Title: 격자 반대로 채우기, Time: 172ms, Memory: 14MB, Status: Passed - CodeTree

--- 240624/격자 반대로 채우기/grid-anti-fill.js
+++ 240624/격자 반대로 채우기/grid-anti-fill.js
@@ -4,7 +4,7 @@ let arr2d = Array(n).fill(0).map(() => Array(n).fill(0));
 let num = 1;
 
 for (let i = n - 1; i >= 0; i--){
-    if (i % 2 === 1){
+    if (i % 2 === (n - 1) % 2){
         for (let j = n - 1; j >= 0; j--){
             arr2d[j][i] = num
             num++
