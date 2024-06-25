1a46bf5d97e6a424ed480c0d02553a5254dd8f44
tacowasabii <godseoril812@gmail.com>
2024-06-25 22:26:38 +0900

    Title: 문자열 범위 출력하기 2, Time: 237ms, Memory: 14MB, Status: Passed - CodeTree

--- 240625/문자열 범위 출력하기 2/print-string-in-range-2.js
+++ 240625/문자열 범위 출력하기 2/print-string-in-range-2.js
@@ -1,5 +1,11 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split("\n");
-let n = Number(input[1])
+let str = input[0];
+let n = Number(input[1]);
 
-console.log(input[0].split("").slice(input[0].length - n, input[0].length).reverse().join(""))
+// if (n > str.length) {
+//   n = str.length;
+// }
+
+let result = str.slice(-n).split("").reverse().join("");
+console.log(result);
