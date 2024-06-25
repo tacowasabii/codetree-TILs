98ffb27401658ec9e697f16bb0334bd4f89c665b
tacowasabii <godseoril812@gmail.com>
2024-06-25 21:55:30 +0900

    Title: 문자열 길이의 합, Time: 261ms, Memory: 29MB, Status: Passed - CodeTree

---
+++ 240625/문자열 길이의 합/sum-length-of-string.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+let n = Number(input[0])
+let sum = 0
+let cnt = 0
+
+for (let i = 1; i <= n; i++){
+    sum += input[i].length
+    if (input[i][0] === 'a')
+        cnt++
+}
+
+console.log(sum, cnt)
