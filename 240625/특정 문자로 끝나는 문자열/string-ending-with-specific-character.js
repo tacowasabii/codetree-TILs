ab421715147443cbc0bd598b93ba066cdd14caab
tacowasabii <godseoril812@gmail.com>
2024-06-25 22:12:23 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 181ms, Memory: 14MB, Status: Passed - CodeTree

--- 240625/특정 문자로 끝나는 문자열/string-ending-with-specific-character.js
+++ 240625/특정 문자로 끝나는 문자열/string-ending-with-specific-character.js
@@ -3,9 +3,10 @@ let input = fs.readFileSync(0).toString().trim().split("\n");
 let flag = true
 
 input.slice(0, 10).forEach((elem) => {
-    if (elem.at(-1) === input[10])
+    if (elem.at(-1) === input[10]){
         console.log(elem)
         flag = false
+    }
 })
 
 if (flag)
