4e108d2fa3fe6fa287c0e987fcbee8bf455c494b
tacowasabii <godseoril812@gmail.com>
2024-06-27 13:27:48 +0900

    Title: 소문자와 숫자, Time: 195ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240627/소문자와 숫자/letter-and-number.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(""); 
+let ans = ""
+
+for (let elem of input){
+    let code = elem.charCodeAt(0);
+
+    if ((code >= 48 && code <= 57) || (code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+      if (code >= 65 && code <= 90) {
+        elem = String.fromCharCode(code + 32);
+      }
+      ans += elem;
+    }
+}
+
+console.log(ans)
