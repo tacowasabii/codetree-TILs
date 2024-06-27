fca5b19945f6b4071eea93afaeb240707dfdbbd3
tacowasabii <godseoril812@gmail.com>
2024-06-27 13:26:01 +0900

    Title: 대문자로 출력하기, Time: 184ms, Memory: 19MB, Status: Passed - CodeTree

--- 240627/대문자로 출력하기/print-in-capital.js
+++ 240627/대문자로 출력하기/print-in-capital.js
@@ -3,8 +3,13 @@ let input = fs.readFileSync(0).toString().trim().split("");
 let ans = ""
 
 for (let elem of input){
-    if (/[a-zA-z]/.test(elem))
-        ans += elem.toUpperCase()
+    let code = elem.charCodeAt(0);
+    if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+      if (code >= 97 && code <= 122) {
+        elem = String.fromCharCode(code - 32);
+      }
+      ans += elem;
+    }
 }
 
 console.log(ans)
