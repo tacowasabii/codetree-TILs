a9be4cb1c5d0677655336c1fded50563b6526843
tacowasabii <godseoril812@gmail.com>
2024-07-01 22:51:17 +0900

    Title: 병합 정렬 구현, Time: 493ms, Memory: 48MB, Status: Passed - CodeTree

---
+++ 240701/병합 정렬 구현/implement-merge-sort.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+let n = Number(input[0]);
+let arr = input[1].split(" ").map(Number);
+
+function merge(left, right) {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const middle = Math.floor(arr.length / 2);
+    const left = arr.slice(0, middle);
+    const right = arr.slice(middle);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+
+console.log(mergeSort(arr).join(" "));
