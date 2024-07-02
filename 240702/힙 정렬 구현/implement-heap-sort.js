9e894edf5b4d831de980c467dc63fb5e5200eee2
tacowasabii <godseoril812@gmail.com>
2024-07-02 22:39:11 +0900

    Title: 힙 정렬 구현, Time: 178ms, Memory: 47MB, Status: Wrong Answer - CodeTree

---
+++ 240702/힙 정렬 구현/implement-heap-sort.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+let n = Number(input[0]);
+let arr = input[1].split(" ").map(Number);
+
+function heapify(arr, n, i) {
+    let largest = i;
+    let left = i * 2
+    let right = i * 2 + 1
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right
+    
+    if (largest !== i){
+        [arr[i], arr[largest]] = [arr[largest], arr[i]]
+        heapify(arr, n, largest)
+    }
+}
+
+function heapSort(arr) {
+    let n = arr.length;
+
+    for (let i = 0; i < Math.floor(n / 2) - 1; i++)
+        heapify(arr, n, i)
+    
+    for (let i = n - 1; i > 0; i--) {
+        [arr[0], arr[i]] = [arr[i], arr[0]]; 
+        heapify(arr, i, 0); 
+    }
+}
+
+heapSort(arr);
+console.log(arr.join(" "));
