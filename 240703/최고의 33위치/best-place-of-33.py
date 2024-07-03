c81b160dbf0dcc3052ab81f72238675716854daa
tacowasabii <godseoril812@gmail.com>
2024-07-03 20:36:41 +0900

    Title: 최고의 33위치, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240703/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,13 @@
+n = int(input())
+matrix = []
+for _ in range(n):
+    matrix.append(list(map(int, input().split(" "))))
+ans = 0
+
+for i in range(n - 2):
+    for j in range(n - 2):
+        Sum = 0
+        for elem in matrix[i:i + 3]:
+            Sum += sum(elem[j:j + 3])
+        ans = max(ans, Sum)
+print(ans)
