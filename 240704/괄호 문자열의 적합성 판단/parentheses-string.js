7f05d108e65f068d7d80d6131523dfa8da1a7043
tacowasabii <godseoril812@gmail.com>
2024-07-04 22:35:36 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 164ms, Memory: 14MB, Status: Passed - CodeTree

--- 240704/괄호 문자열의 적합성 판단/parentheses-string.js
+++ 240704/괄호 문자열의 적합성 판단/parentheses-string.js
@@ -1,20 +1,21 @@
 const fs = require("fs")
 const input = fs.readFileSync(0).toString().trim().split("")
 let stack = []
+let flag = true
 
 for (let elem of input){
     if (elem === '(')
         stack.push(elem)
     else {
         if (stack.length === 0){
-            console.log("No")
+            flag = false
             break
         }
         stack.pop()
     }
 }
 
-if (stack.length === 0)
+if (stack.length === 0 && flag)
     console.log("Yes")
 else
     console.log("No")
