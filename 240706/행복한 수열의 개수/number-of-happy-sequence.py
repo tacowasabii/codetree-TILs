71ef183eaa04e82afa56b69abc5223b16cc8e5c5
tacowasabii <godseoril812@gmail.com>
2024-07-06 00:56:07 +0900

    Title: 행복한 수열의 개수, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240706/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,29 @@
+n, m = map(int, input().split())
+matrix = []
+for _ in range(n):
+    matrix.append(list(map(int, input().split())))
+ans = 0
+
+for i in range(n):
+    cnt = 1
+    maxCnt = 0
+    for j in range(1, n):
+        if matrix[i][j] != matrix[i][j - 1]:
+            cnt = 1
+        else:
+            cnt += 1
+        maxCnt = max(cnt, maxCnt)
+    if maxCnt >= m:
+        ans += 1
+    
+    cnt = 1
+    maxCnt = 0
+    for j in range(1, n):
+        if matrix[i][j] != matrix[i][j - 1]:
+            cnt = 1
+        else:
+            cnt += 1
+        maxCnt = max(cnt, maxCnt)
+    if maxCnt >= m:
+        ans += 1
+print(ans)
