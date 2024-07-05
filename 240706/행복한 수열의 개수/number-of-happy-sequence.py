176166c2f1805555c927882041bdc195fdf275b4
tacowasabii <godseoril812@gmail.com>
2024-07-06 00:59:05 +0900

    Title: 행복한 수열의 개수, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

--- 240706/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240706/행복한 수열의 개수/number-of-happy-sequence.py
@@ -19,7 +19,7 @@ for i in range(n):
     cnt = 1
     maxCnt = 1
     for j in range(1, n):
-        if matrix[i][j] != matrix[i][j - 1]:
+        if matrix[j][i] != matrix[j - 1][i]:
             cnt = 1
         else:
             cnt += 1
