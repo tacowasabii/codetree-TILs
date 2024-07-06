038e9d3bbeba3585287a5c78fc0a90d7fc5a30c1
tacowasabii <godseoril812@gmail.com>
2024-07-06 23:57:11 +0900

    Title: 양수 직사각형의 최대 크기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240706/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.js
+++ 240706/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.js
@@ -2,6 +2,9 @@ const fs = require("fs")
 const input = fs.readFileSync(0).toString().trim().split("\n")
 const [n, m] = input[0].split(" ")
 const table = input.slice(1).map(line => line.split(" ").map(Number));
+[3, 6, 7, -4, 1],
+[6, 1, 8, 15, -5,],
+[3, -5, 1, 16, 3]]
 let ans = 0
 
 for (let i = 1; i <= n; i++){
@@ -10,7 +13,7 @@ for (let i = 1; i <= n; i++){
             for (let col = 0; col < m - j + 1; col++){
                 flag = true
                 for (let k = row; k < row + i; k++){
-                    for (let l = col; l > col + j; l++){
+                    for (let l = col; l < col + j; l++){
                         if (table[k][l] <= 0)
                             flag = false
                     }
