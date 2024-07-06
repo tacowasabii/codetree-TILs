c2db3e693b9ec1b2904a923b231a3536505a919f
tacowasabii <godseoril812@gmail.com>
2024-07-07 00:05:14 +0900

    Title: 양수 직사각형의 최대 크기, Time: 181ms, Memory: 18MB, Status: Passed - CodeTree

---
+++ 240707/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.js
@@ -0,0 +1,24 @@
+const fs = require("fs")
+const input = fs.readFileSync(0).toString().trim().split("\n")
+const [n, m] = input[0].split(" ")
+const table = input.slice(1).map(line => line.split(" ").map(Number));
+let ans = -1
+
+for (let i = 1; i <= n; i++){
+    for (let j = 1; j <= m; j++){
+        for (let row = 0; row < n - i + 1; row++){
+            for (let col = 0; col < m - j + 1; col++){
+                flag = true
+                for (let k = row; k < row + i; k++){
+                    for (let l = col; l < col + j; l++){
+                        if (table[k][l] <= 0)
+                            flag = false
+                    }
+                }
+                if (flag)
+                    ans = Math.max(ans, i * j)
+            }
+        }
+    }
+}
+console.log(ans)
