334af97823397963bbf7b7d40cff30684d1d8421
tacowasabii <godseoril812@gmail.com>
2024-07-12 00:14:06 +0900

    Title: 최적의 십자 모양 폭발, Time: 268ms, Memory: 22MB, Status: Wrong Answer - CodeTree

--- 240712/최적의 십자 모양 폭발/best-cross-shape-bomb.js
+++ 240712/최적의 십자 모양 폭발/best-cross-shape-bomb.js
@@ -1,21 +1,22 @@
 function bomb(arr, n, row, col){
-    power = arr[row][col]
+    arr_copy = JSON.parse(JSON.stringify(arr))
+    power = arr_copy[row][col]
     for (let i = 0; i < n; i++){
         for (let j = 0; j < n; j++){
             if ((i > row - power && i < row + power && j === col) || (j > col - power && j < col + power && i === row))
-                arr[i][j] = 0
+                arr_copy[i][j] = 0
         }
     }
 
     for (let i = n - 1; i > 0; i--){
         for(let j = 0; j < n; j++){
-            if (arr[i][j] == 0){
-                arr[i][j] = arr[i - 1][j]
-                arr[i - 1][j] = 0
+            if (arr_copy[i][j] == 0){
+                arr_copy[i][j] = arr_copy[i - 1][j]
+                arr_copy[i - 1][j] = 0
             }
         }
     }
-    return arr
+    return arr_copy
 }
 
 const fs = require("fs")
@@ -27,7 +28,7 @@ let new_arr
 
 for (let i = 0; i < n; i++){
     for (let j = 0; j < n; j++){
-        new_arr = bomb(arr, n, i, j)
+        new_arr = bomb(arr.slice(), n, i, j)
         let cnt = 0
         for (let i = 0; i < n; i++){
             for (let j = 0; j < n - 1; j++){
