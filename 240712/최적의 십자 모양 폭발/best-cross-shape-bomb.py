3dc780dbe9728b150e8ace09f233377fffdd788a
tacowasabii <godseoril812@gmail.com>
2024-07-12 23:53:33 +0900

    Title: 최적의 십자 모양 폭발, Time: 1616ms, Memory: 31MB, Status: Passed - CodeTree

--- 240712/최적의 십자 모양 폭발/best-cross-shape-bomb.py
+++ 240712/최적의 십자 모양 폭발/best-cross-shape-bomb.py
@@ -3,31 +3,36 @@ from copy import deepcopy
 def bomb(arr, n, row, col):
     new_arr = deepcopy(arr)
     power = int(new_arr[row][col])
+    
+    for i in range(n):
+        for j in range(n):
+            if ((i > row - power and i < row + power and j == col) or (j > col - power and j < col + power and i == row)):
+                new_arr[i][j] = '0'
+    
     for j in range(n):
-        tmp = ""
+        tmp = []
         for i in range(n):
-            if not ((i == row and j > col - power and j < col + power) or (j == col and i > row - power and i < row + power)):
-                tmp += new_arr[i][j]
-        tmp = '0' * (n - len(tmp)) + tmp
-
+            if new_arr[i][j] != '0':
+                tmp.append(new_arr[i][j])
+        tmp = ['0'] * (n - len(tmp)) + tmp
         for i in range(n):
             new_arr[i][j] = tmp[i]
     
     return new_arr
 
 n = int(input())
-arr = [input().split(" ") for _ in range(n)]
+arr = [input().split() for _ in range(n)]
 ans = 0
 
 for i in range(n):
     for j in range(n):
         new_arr = bomb(arr, n, i, j)
         cnt = 0
-        for k in range(n):
-            for v in range(n - 1):
-                if new_arr[k][v] != '0' and new_arr[k][v] == new_arr[k][v + 1]:
+        for x in range(n):
+            for y in range(n - 1):
+                if new_arr[x][y] != '0' and new_arr[x][y] == new_arr[x][y + 1]:
                     cnt += 1
-                if new_arr[v][k] != '0' and new_arr[v][k] == new_arr[v + 1][k]:
+                if new_arr[y][x] != '0' and new_arr[y][x] == new_arr[y + 1][x]:
                     cnt += 1
         ans = max(ans, cnt)
 
