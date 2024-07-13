47db049e5d7fb0ce18f70e00359e71498da74927
tacowasabii <godseoril812@gmail.com>
2024-07-13 18:17:03 +0900

    Title: 핀볼게임, Time: 167ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240713/핀볼게임/pinball-game.js
+++ 240713/핀볼게임/pinball-game.js
@@ -38,6 +38,8 @@ function time(x, y, dir){
     return t
 }
 
+let ans = 0
+
 for (let i = 0; i < n; i++){
     ans = Math.max(time(i, 0, [1, 0]), time(n - 1, 0, [0, 1]), time(i, n - 1, [-1, 0]), time(0, i, [1, 0]), ans)
 }
