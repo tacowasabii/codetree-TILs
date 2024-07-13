71b5263ddd1b0c86cb22663edd261b6420121f32
tacowasabii <godseoril812@gmail.com>
2024-07-13 18:16:19 +0900

    Title: 핀볼게임, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240713/핀볼게임/pinball-game.js
+++ 240713/핀볼게임/pinball-game.js
@@ -39,6 +39,6 @@ function time(x, y, dir){
 }
 
 for (let i = 0; i < n; i++){
-    ans = Math.max(time(i, 0, [1, 0]), time(n - 1, 0, [0, 1]), time(i, n - 1, [-1, 0]), time(0, i, [1, 0]))
+    ans = Math.max(time(i, 0, [1, 0]), time(n - 1, 0, [0, 1]), time(i, n - 1, [-1, 0]), time(0, i, [1, 0]), ans)
 }
 console.log(ans)
