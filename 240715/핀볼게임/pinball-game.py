d155b88be56c3f5e52d7baf9c35a2aedee02b13f
tacowasabii <godseoril812@gmail.com>
2024-07-15 21:46:33 +0900

    Title: 핀볼게임, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240715/핀볼게임/pinball-game.py
@@ -0,0 +1,30 @@
+n = int(input())
+grid = [list(map(int, input().split(" "))) for _ in range(n)]
+
+def changePosition(x, y, direction):
+    dx = [1, 0, -1, 0]
+    dy = [0, -1, 0, 1]
+
+    if grid[x][y] == 2:
+        direction = 3 - direction
+    elif grid[x][y] == 1:
+        if direction == 0 or direction == 2:
+            direction += 1
+        else:
+            direction -= 1
+    x += dx[direction]
+    y += dy[direction]
+    
+    return x, y, direction
+
+def calcTime(x, y, direction):
+    time = 1
+    while(x >= 0 and x < n and y >= 0 and y < n):
+        x, y, direction = changePosition(x, y, direction)
+        time += 1
+    return time
+
+for i in range(n):
+    time = max(calcTime(0, i, 0), calcTime(i, n - 1, 1), calcTime(n - 1, i, 2), calcTime(i, 0, 3))
+
+print(time)
