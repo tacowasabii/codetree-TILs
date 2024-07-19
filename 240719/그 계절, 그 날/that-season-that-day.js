dd9e469927cafec7bddb6674db63ef8364c63a61
tacowasabii <godseoril812@gmail.com>
2024-07-19 09:38:42 +0900

    Title: 그 계절, 그 날, Time: 162ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240719/그 계절, 그 날/that-season-that-day.js
@@ -0,0 +1,47 @@
+function isYunNeyon(year){
+    if (year % 4 === 0){
+        if (year % 100 === 0){
+            if (year % 400 === 0){
+                return true
+            }
+            return false
+        }
+        return true
+    }
+    return false
+}
+
+function daysOfMonth(month, isYunNeyon){
+    more = [1, 3, 5, 7, 8, 10, 12]
+    if (month === 2){
+        if (isYunNeyon){
+            return 29
+        }
+        return 28
+    } else if (month in more) {
+        return 31
+    } else {
+        return 30
+    }
+}
+
+function season(month){
+    if (3 <= month && month <= 5){
+        return "Spring"
+    } else if (6 <= month && month <= 8){
+        return "Summer"
+    } else if (9 <= month && month <= 11){
+        return "Fall"
+    } else {
+        return "Winter"
+    }
+}
+
+const fs = require("fs")
+const [y, m, d] = fs.readFileSync(0).toString().trim().split(" ").map(Number)
+
+if (d <= daysOfMonth(m, isYunNeyon(y))){
+    console.log(season(m))
+}
+
+return -1
