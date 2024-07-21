9d3200996951cd1177e6e73538154d7d88d4d4ac
tacowasabii <godseoril812@gmail.com>
2024-07-21 22:00:19 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 246ms, Memory: 14MB, Status: Passed - CodeTree

--- 240721/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.js
+++ 240721/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.js
@@ -1,31 +1,18 @@
 const fs = require("fs")
 const n = Number(fs.readFileSync(0).toString().trim())
 
-function starDesc(n){
-    if (n == 1){
-        console.log("*")
+function star(n){
+    if (n == 0){
         return
     }
-    result = ""
+    let result = ""
     for (let i = 0; i < n; i++){
         result += "* "
     }
     console.log(result)
-    starDesc(n - 1)
-}
-
-function starAsc(j, n){
-    result = ""
-    for (let i = 0; i < j; i++){
-        result += "* "
-    }
+    star(n - 1)
     console.log(result)
-    if (j == n){
-        return
-    }
-    starAsc(j + 1, n)
 }
 
 
-starDesc(n)
-starAsc(1, n)
+star(n)
