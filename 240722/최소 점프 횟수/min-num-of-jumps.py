2fbf68985786bc301f9dbda83a6fca1d298dd343
tacowasabii <godseoril812@gmail.com>
2024-07-22 22:30:11 +0900

    Title: 최소 점프 횟수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240722/최소 점프 횟수/min-num-of-jumps.py
@@ -0,0 +1,18 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+stack = [[0, 0]]
+ans = float('inf')
+
+while stack:
+    [idx, num] = stack.pop()
+    if idx == n - 1:
+        ans = min(ans, num)
+    else:
+        for i in range(1, arr[idx] + 1):
+            stack.append([idx + i, num + 1])
+
+if ans == float('inf'):
+    print(-1)
+else:
+    print(ans)
