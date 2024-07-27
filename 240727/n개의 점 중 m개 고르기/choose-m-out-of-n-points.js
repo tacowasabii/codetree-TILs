37b9f343e7d9de72078e5c00a8748ae3253b8c41
tacowasabii <godseoril812@gmail.com>
2024-07-27 22:50:50 +0900

    Title: n개의 점 중 m개 고르기, Time: 369ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 240727/n개의 점 중 m개 고르기/choose-m-out-of-n-points.js
@@ -0,0 +1,36 @@
+function* combination(arr, m) {
+    if (m === 0) {
+        yield [];
+        return;
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        let num = arr[i];
+        let rest_arr = arr.slice(i + 1);
+        for (let elem of combination(rest_arr, m - 1)) {
+            yield [num].concat(elem);
+        }
+    }
+}
+
+function distance(point1, point2){
+    return (point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2
+}
+
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+const [n, m] = input[0].split(" ").map(Number);
+const points = input.slice(1).map(point => point.split(" ").map(Number));
+let ans = Number.MAX_SAFE_INTEGER
+
+for (let comb of combination(points, m)){
+    dist = 0
+    for (let i = 0; i < comb.length; i++){
+        for (let j = i + 1; j < comb.length; j++){
+            dist = Math.max(dist, distance(points[i], points[j]))
+        }
+    }
+    ans = Math.min(ans, dist)
+}
+
+console.log(ans)
