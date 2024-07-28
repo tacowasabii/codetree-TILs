c29454bbe7bf1ecd0ece2d5e1f0a6c5f3ba01fda
tacowasabii <godseoril812@gmail.com>
2024-07-28 14:17:46 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240728/수들 중 최솟값 최대화하기/maximin-of-numbers.py
@@ -0,0 +1,21 @@
+from itertools import permutations
+
+n = int(input())
+matrix = []
+for _ in range(n):
+    matrix.append(list(map(int, input().split())))
+
+perms = list(permutations([i for i in range(n)], n))
+
+ans = 0
+
+for perm in perms:
+    points = []
+    for i in range(n):
+        points.append([perm[i], i])
+    num = 10
+    for point in points:
+        num = min(num, matrix[point[0]][point[1]])
+    ans = max(ans, num)
+
+print(ans)
