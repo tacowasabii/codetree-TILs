1593604598e36883f0af3a0bb6b2529ffac8ee38
tacowasabii <godseoril812@gmail.com>
2024-08-02 14:05:25 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 193ms, Memory: 15MB, Status: Passed - CodeTree

--- 240802/T를 초과하는 연속 부분 수열/subsequence-above-t.js
+++ 240802/T를 초과하는 연속 부분 수열/subsequence-above-t.js
@@ -1,7 +1,7 @@
 const fs = require("fs")
 const input = fs.readFileSync(0).toString().trim().split("\n")
-const [n, t] = input[0].split(" ")
-const nums = input[1].split(" ")
+const [n, t] = input[0].split(" ").map(Number)
+const nums = input[1].split(" ").map(Number)
 let max = 0
 
 for (let i = 0; i < n; i++){
