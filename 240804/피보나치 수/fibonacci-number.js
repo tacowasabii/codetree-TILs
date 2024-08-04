ae6baea2b219447bdaa45d96a764d2f4d8a4ebe6
tacowasabii <godseoril812@gmail.com>
2024-08-04 09:21:50 +0900

    Title: 피보나치 수, Time: 265ms, Memory: 14MB, Status: Passed - CodeTree

--- 240804/피보나치 수/fibonacci-number.js
+++ 240804/피보나치 수/fibonacci-number.js
@@ -1,11 +1,13 @@
-function pibonacci(n){
-    if (n <= 2){
-        return 1
-    }
-    return pibonacci(n - 2) + pibonacci(n - 1)
-}
-
 const fs = require("fs")
 const n = Number(fs.readFileSync(0).toString().trim())
 
-console.log(pibonacci(n))
+const pibo = [0, 1, 1]
+
+if (n <= 2){
+    console.log(pibo[n])
+} else{
+    for (let i = 3; i <= n; i++){
+        pibo.push(pibo[i - 1] + pibo[i - 2])
+    }
+    console.log(pibo[n])
+}
