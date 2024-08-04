449509836dc5ab0a312041cafe2b13ff0aaaa49f
tacowasabii <godseoril812@gmail.com>
2024-08-04 09:26:22 +0900

    Title: 계단 오르기, Time: 181ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240804/계단 오르기/climbing-stairs.js
@@ -0,0 +1,10 @@
+const fs = require("fs")
+const n = Number(fs.readFileSync(0).toString().trim())
+
+const dp = [0, 0, 1, 1]
+
+for (let i = 4; i <= n; i++){
+    dp.push(dp[i - 2] + dp[i - 3])
+}
+
+console.log(dp[n])
