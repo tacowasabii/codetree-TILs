9681761cd2acf4fbe9905bec5f471a273d0b4b0d
tacowasabii <godseoril812@gmail.com>
2024-08-04 09:17:31 +0900

    Title: 피보나치 수, Time: 179ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240804/피보나치 수/fibonacci-number.js
@@ -0,0 +1,11 @@
+function pibonacci(n){
+    if (n <= 2){
+        return 1
+    }
+    return pibonacci[n - 2] + pibonacci[n - 1]
+}
+
+const fs = require("fs")
+const n = Number(fs.readFileSync(0).toString().trim())
+
+console.log(pibonacci(n))
